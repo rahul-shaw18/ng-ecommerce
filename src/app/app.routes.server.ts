278269e1492038f4ef93ecace3a7a492570ce727
Rahul Shaw <rahul.shaw505@gmail.com>
2026-02-25 21:19:58 +0530

    SSR and Prerender/SSG

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,37 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { inject } from '@angular/core';
+import { EcommerceStore } from './ecommerce-store';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'products/:category',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const store = inject(EcommerceStore);
+      const categories = store.categories();
+      return categories.map((cat) => ({ category: cat }));
+    },
+  },
+  {
+    path: 'wishlist',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'cart',
+    renderMode: RenderMode.Client,
+  },
+
+  {
+    path: 'checkout',
+    renderMode: RenderMode.Client,
+  },
+
+  {
+    path: 'order-success',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Client
-  }
+    renderMode: RenderMode.Server,
+  },
 ];
